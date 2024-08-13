ac66790672f835b3c12fdecd5bd9a2801b4d44d2
crststnsc <cstanescu143@gmail.com>
2024-08-13 20:04:30 +0300

    initialize memory and write to memory functions

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 
 
+const Flags = enum(u3){
+    FLAG_Z = 7,
+    FLAG_N = 6,
+    FLAG_H = 5,
+    FLAG_C = 4
+};
+
+
 const Register = struct {
     reg : u16,
     
@@ -15,14 +23,97 @@ const Register = struct {
 
 
 const Emulator = struct {
-    screen_data: [160][144][3]u8,
-    memory     : [0x10000]u8,
+    screen_data     : [160][144][3]u8,
+    memory          : [0x10000]u8,
+    program_counter : u16,
 
+    // The stack pointer is modeled as a register 
+    // because some opcodes use the hi and lo bits of the stack pointer
+    stack_pointer   : Register,
+
+    // Registers
+    register_af     : Register,
+    register_bc     : Register,
+    register_de     : Register,
+    register_hl     : Register,
+    
+    
     pub fn update() void {
         // const max_cycles = 69905;
         // var cycles_this_update = 0;
 
         // TODO
+    }
+
+    pub fn init(self: *Emulator) Emulator {
+        // when the emulator starts we must set the state of registers,
+        // the stack pointer, program counter and some memory registers
+        self.program_counter = 0x100;
+        self.stack_pointer   = 0xFFFE;
+
+        self.init_memory();
+        self.init_registers();
+    }
+
+    fn init_memory(self: *Emulator) void {
+        self.memory[0xFF05] = 0x00;
+        self.memory[0xFF06] = 0x00;
+        self.memory[0xFF07] = 0x00;
+        self.memory[0xFF10] = 0x80;
+        self.memory[0xFF11] = 0xBF;
+        self.memory[0xFF12] = 0xF3;
+        self.memory[0xFF14] = 0xBF;
+        self.memory[0xFF16] = 0x3F;
+        self.memory[0xFF17] = 0x00;
+        self.memory[0xFF19] = 0xBF;
+        self.memory[0xFF1A] = 0x7F;
+        self.memory[0xFF1B] = 0xFF;
+        self.memory[0xFF1C] = 0x9F;
+        self.memory[0xFF1E] = 0xBF;
+        self.memory[0xFF20] = 0xFF;
+        self.memory[0xFF21] = 0x00;
+        self.memory[0xFF22] = 0xFF;
+        self.memory[0xFF23] = 0xBF;
+        self.memory[0xFF24] = 0x77;
+        self.memory[0xFF25] = 0xF3;
+        self.memory[0xFF26] = 0xF1;
+        self.memory[0xFF40] = 0x91;
+        self.memory[0xFF42] = 0x00;
+        self.memory[0xFF43] = 0x00;
+        self.memory[0xFF45] = 0x00;
+        self.memory[0xFF47] = 0xFC;
+        self.memory[0xFF48] = 0xFF;
+        self.memory[0xFF49] = 0xFF;
+        self.memory[0xFF4A] = 0x00;
+        self.memory[0xFF4B] = 0x00;
+        self.memory[0xFFFF] = 0x00;
+
+    }
+
+    fn init_registers(self: *Emulator) void {
+        self.register_af = 0x01B0;
+        self.register_bc = 0x0013;
+        self.register_de = 0x00D8;
+        self.register_hl = 0x014D;
+    }
+
+    fn write_memory(self: *Emulator, address: u16, data: u8) void {
+        // don't allow to write to read-only memory
+        if (address < 0x8000) {
+            // TODO some warning
+        }
+        // restricted memory area
+        else if (address >= 0xFEA0 and address < 0xFEFF) {
+            // TODO some warning
+        }
+        // anything written to ECHO also gets written in RAM 
+        else if (address >= 0xE000 and address < 0xFE00) {
+            self.memory[address] = data;
+            self.write_memory(address - 0x2000, data);
+        }
+        else{
+            self.memory[address] = data;
+        }
     }
 };
 
@@ -39,5 +130,6 @@ pub fn main() !void {
     var reg: Register = Register {.reg = 0xAABB};
     std.debug.print("Register value: {x}\n", .{reg.reg});
     std.debug.print("Lo value: {x}\n", .{reg.lo().*});
-    std.debug.print("Hi value: {x}\n", .{reg.hi_alt().*});
+    std.debug.print("Hi value: {x}\n", .{reg.hi().*});
+
 }
